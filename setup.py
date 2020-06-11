674df90473035595503ff889b198db4319a53e29
Andy Lulham <a.lulham@gmail.com>
2020-06-11 21:53:39 +0100

    v0.0.8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/FullFact/python-batchmailchimp',
     author='Andy Lulham',
     author_email='andy.lulham@fullfact.org',
-    version='0.0.7',
+    version='0.0.8',
     packages=find_packages(),
     license='MIT',
     keywords='mailchimp api v3 client wrapper',
